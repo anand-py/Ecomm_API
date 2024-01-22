8118970d18eb8347453a878c372ceba29297e2b9
news_aggregator_api <aanand.py@gmail.com>
2024-01-22 18:11:56 +0530

    deploy

--- src/util/database.js
+++ src/util/database.js
@@ -1,3 +1,4 @@
+/* This code is setting up a connection to a MySQL database using the Sequelize library in JavaScript. */
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('ecommerce', 'root', 'root', {
